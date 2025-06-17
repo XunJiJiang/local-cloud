c91d008717b560a6f0d13648a822ddd704c89ed9
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2025-06-17 14:25:13 +0800

    init(nuxt)

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxt/scripts',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    '@nuxt/content',
+  ],
+});
