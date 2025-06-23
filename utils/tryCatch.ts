93b809f670a3fb25bb67bbeb4223bc3eb5306a2a
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2025-06-23 23:07:49 +0800

    feat: tryCatch 支持捕获移步错误

--- utils/tryCatch.ts
+++ utils/tryCatch.ts
@@ -1,6 +1,34 @@
-export default function <T>(callback: () => T): [T, undefined] | [undefined, Error] {
+const isPromiseLike = <T>(value: T | Promise<T>): value is Promise<T> => {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    'then' in value &&
+    typeof value.then === 'function'
+  )
+}
+
+export default function tryCatch<T>(
+  callback: () => Promise<T>
+): Promise<[T, undefined] | [undefined, Error]>
+export default function tryCatch<T>(callback: () => T): [T, undefined] | [undefined, Error]
+export default function tryCatch<T>(
+  callback: () => T | Promise<T>
+): Promise<[T, undefined] | [undefined, Error]> | [T, undefined] | [undefined, Error] {
   try {
     const result = callback()
+
+    if (isPromiseLike(result)) {
+      return result
+        .then((res) => [res, undefined] as [T, undefined])
+        .catch(
+          (err) =>
+            [undefined, err instanceof Error ? err : new Error('Unknown error')] as [
+              undefined,
+              Error
+            ]
+        )
+    }
+
     return [result, void 0]
   } catch (error) {
     if (error instanceof Error) {
