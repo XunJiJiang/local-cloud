43be0974b013f65b187961065f9c9870e7553ac0
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2025-06-25 15:52:45 +0800

    feat: image preview 添加 load 和 error 事件，暴露 scrollIntoView 方法

--- components/preview/image.vue
+++ components/preview/image.vue
@@ -3,10 +3,29 @@ const props = defineProps<{
   root: string
   path: string[]
 }>()
-const src = `/api/preview-images?root=${encodeURIComponent(props.root)}&path=${encodeURIComponent(props.path.join('/'))}`
+
+const emits = defineEmits<{
+  error: [Event]
+  load: [Event]
+}>()
+
+const containerRef = useTemplateRef('container-ref')
+
+defineExpose({
+  scrollIntoView: (opt?: boolean | ScrollIntoViewOptions) => {
+    containerRef.value?.scrollIntoView(opt)
+  }
+})
+
+const src = `/api/preview-images?root=${encodeURIComponent(props.root)}&path=${props.path.map(encodeURIComponent).join('/')}`
 </script>
 <template>
-  <div class="flex items-center justify-center overflow-hidden">
-    <img :src="src" class="max-w-full object-contain max-h-[calc(100vh-94px)]" />
+  <div ref="container-ref" class="flex items-center justify-center overflow-hidden">
+    <img
+      :src="src"
+      class="max-w-screen object-contain max-h-[calc(100vh-94px)] h-[calc(100vh-94px)]"
+      @error="emits('error', $event)"
+      @load="emits('load', $event)"
+    />
   </div>
 </template>
