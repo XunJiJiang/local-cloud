167986eea1298747dec98cf500c91ac4ab43f753
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2025-06-30 03:44:05 +0800

    feat: 移除文件树内图片预览功能

--- pages/tree/[...path].vue
+++ pages/tree/[...path].vue
@@ -52,68 +52,11 @@ const sortedItems = [
     })
 ]
 
-/** 当前目录只有一个子文件夹，且这个子文件夹内都是图片 */
-const [isSingleFolderWithImages, childFiles] = await (async () => {
-  if (data.value?.folders.length !== 1 || data.value?.files.length !== 0) return [false, []]
-
-  const [childData, error] = await tryCatch(
-    async () =>
-      await $fetch('/api/list-folder-files', {
-        method: 'POST',
-        body: {
-          root: paramPath.value[0] ?? '',
-          path: paramPath.value
-            .slice(1)
-            .concat(data.value?.folders[0].name ?? '')
-            .filter(Boolean)
-        }
-      })
-  )
-  if (error) return [false, []]
-  if (childData.folders.length !== 0) return [false, []]
-
-  return [
-    childData.files.every((file: { name: string }) =>
-      /\.(jpg|jpeg|png|gif|webp)$/i.test(file.name)
-    ),
-    childData.files.sort((a: { name: string }, b: { name: string }) => a.name.localeCompare(b.name))
-  ]
-})()
-
 const headerNav = useHeaderNav()
 
-const previewImages = useTemplateRef('preview-image')
-
-const scrollToFirstImage = debounce(() => {
-  if (isSingleFolderWithImages && previewImages.value) {
-    previewImages.value[0]?.scrollIntoView()
-  }
-}, 100)
-
 onMounted(() => {
-  if (isSingleFolderWithImages) {
-    // 如果是单个文件夹且里面都是图片，使用 store 保存滚动位置
-    headerNav.setHeaderNav([
-      {
-        key: 'preview-in-tree',
-        label: '>',
-        type: 'button'
-      }
-    ])
-    scrollToFirstImage()
-  } else {
     headerNav.setHeaderNav([])
-  }
 })
-
-if (isSingleFolderWithImages) {
-  watch(
-    () => headerNav.headerNavEmit?.get('preview-in-tree'),
-    (v) => {
-      if (typeof v === 'number') scrollToFirstImage()
-    }
-  )
-}
 
 if (error.value) {
   throw createError(error.value)
@@ -128,17 +71,5 @@ if (error.value) {
       :root="encodeURIComponent(paramPath[0])"
       :path="[...paramPath.slice(1).map(encodeURIComponent), 'README.md']"
     />
-    <div v-if="isSingleFolderWithImages" class="my-6 overflow-x-auto max-w-full">
-      <div class="inline-flex flex-row-reverse overflow-x-auto">
-        <PreviewImage
-          v-for="item in childFiles"
-          ref="preview-image"
-          :key="item.name"
-          :root="encodeURIComponent(paramPath[0])"
-          :path="paramPath.slice(1).concat([data?.folders[0].name ?? '', item.name])"
-          @load="scrollToFirstImage"
-        />
-      </div>
-    </div>
   </div>
 </template>
