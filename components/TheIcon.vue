2d70ed96c2118492763375698d34de7780a87cf2
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2025-07-06 22:20:26 +0800

    feat: 添加图标

---
+++ components/TheIcon.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: Number,
+    default: 16
+  },
+  color: {
+    type: String,
+    default: '#000'
+  },
+  style: {
+    type: Object,
+    default: () => ({})
+  }
+})
+</script>
+
+<template>
+  <span
+    :class="{
+      iconfont: true,
+      [`icon-${name}`]: true
+    }"
+    :style="{ fontSize: size + 'px', color: color, ...style }"
+  ></span>
+</template>
+
+<style scoped lang="scss">
+@use '~/assets/icon/iconfont.css';
+</style>
